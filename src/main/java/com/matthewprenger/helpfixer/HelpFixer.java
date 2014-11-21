fb2b2f82850f89503ec1a406bc120ed6ebdc9a0b
Matthew Prenger <matthewprenger@gmail.com>
2014-11-20 19:23:41 -0600

    Pull command name nullcheck from @Aedda, added logging for commands that have an invalid compareTo method.

--- src/main/java/com/matthewprenger/helpfixer/HelpFixer.java
+++ src/main/java/com/matthewprenger/helpfixer/HelpFixer.java
@@ -1,15 +1,17 @@
 package com.matthewprenger.helpfixer;
 
+import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLServerStartedEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
+import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandHelp;
 import net.minecraft.command.ICommand;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.server.MinecraftServer;
 
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import javax.annotation.Nonnull;
+import java.util.*;
 
 @Mod(modid = HelpFixer.MOD_ID, name = HelpFixer.MOD_ID, acceptableRemoteVersions = "*")
 public class HelpFixer {
@@ -18,13 +20,22 @@ public class HelpFixer {
 
     @Mod.EventHandler
     public void serverStarting(FMLServerStartingEvent event) {
-
         event.registerServerCommand(new CommandHelp() {
 
             @SuppressWarnings("unchecked")
             @Override
             protected List<ICommand> getSortedPossibleCommands(ICommandSender sender) {
                 List<ICommand> list = MinecraftServer.getServer().getCommandManager().getPossibleCommands(sender);
+
+                Iterator<ICommand> iterator = list.iterator();
+                while (iterator.hasNext()) {
+                    ICommand command = iterator.next();
+                    if (command.getCommandName() == null) {
+                        FMLLog.warning(String.format("[HelpFixer] Identified command with null name, Ignoring: %s", command.getClass().getName()));
+                        iterator.remove();
+                    }
+                }
+
                 Collections.sort(list, new Comparator<ICommand>() {
 
                     @Override
@@ -37,4 +48,59 @@ public class HelpFixer {
 
         });
     }
+
+    @SuppressWarnings("unchecked")
+    @Mod.EventHandler
+    public void serverStarted(final FMLServerStartedEvent event) {
+        Collection<ICommand> commands = MinecraftServer.getServer().getCommandManager().getCommands().values();
+
+        for (final ICommand command : commands) {
+            if (!(validCompareTo(command))) {
+                FMLLog.warning(String.format("[HelpFixer] Command %s incorrectly overrides compareTo: %s", command.getCommandName(), command.getClass().getName()));
+            }
+        }
+    }
+
+    /**
+     * Checks to see if an {@link net.minecraft.command.ICommand ICommand} has a valid compareTo method
+     *
+     * @param command the command
+     * @return {@code true} if the compareTo method is valid, {@code false} if not
+     */
+    @SuppressWarnings("unchecked")
+    public static boolean validCompareTo(@Nonnull ICommand command) {
+        return command.compareTo(testCmd1) != command.compareTo(testCmd2);
+    }
+
+    private static final ICommand testCmd1 = new CommandBase() {
+        @Override
+        public String getCommandName() {
+            return "a";
+        }
+
+        @Override
+        public String getCommandUsage(ICommandSender sender) {
+            return null;
+        }
+
+        @Override
+        public void processCommand(ICommandSender sender, String[] args) {
+        }
+    };
+
+    private static final ICommand testCmd2 = new CommandBase() {
+        @Override
+        public String getCommandName() {
+            return "z";
+        }
+
+        @Override
+        public String getCommandUsage(ICommandSender sender) {
+            return null;
+        }
+
+        @Override
+        public void processCommand(ICommandSender sender, String[] args) {
+        }
+    };
 }
---
+++ src/test/java/com/matthewprenger/helpfixer/HelpFixerTest.java
@@ -0,0 +1,78 @@
+package com.matthewprenger.helpfixer;
+
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.ICommand;
+import net.minecraft.command.ICommandSender;
+import org.junit.Test;
+
+import static com.matthewprenger.helpfixer.HelpFixer.validCompareTo;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class HelpFixerTest {
+
+    @Test
+    public void testValidCompareTo() {
+        ICommand cmd1 = new CommandBase() {
+            @Override
+            public String getCommandName() {
+                return "foo";
+            }
+
+            @Override
+            public String getCommandUsage(ICommandSender sender) {
+                return null;
+            }
+
+            @Override
+            public void processCommand(ICommandSender sender, String[] args) {
+            }
+
+            @Override
+            public int compareTo(Object p_compareTo_1_) {
+                return 0;
+            }
+        };
+
+        ICommand cmd2 = new CommandBase() {
+            @Override
+            public String getCommandName() {
+                return "foo";
+            }
+
+            @Override
+            public String getCommandUsage(ICommandSender sender) {
+                return null;
+            }
+
+            @Override
+            public void processCommand(ICommandSender sender, String[] args) {
+            }
+
+            @Override
+            public int compareTo(ICommand p_compareTo_1_) {
+                return 0;
+            }
+        };
+
+        ICommand cmd3 = new CommandBase() {
+            @Override
+            public String getCommandName() {
+                return "z";
+            }
+
+            @Override
+            public String getCommandUsage(ICommandSender sender) {
+                return null;
+            }
+
+            @Override
+            public void processCommand(ICommandSender sender, String[] args) {
+            }
+        };
+
+        assertFalse(validCompareTo(cmd1));
+        assertFalse(validCompareTo(cmd2));
+        assertTrue(validCompareTo(cmd3));
+    }
+}
